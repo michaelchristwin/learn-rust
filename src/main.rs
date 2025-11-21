3487c6cee9958f702868e61772b4c2b2da7a8fbb
michaelchristwin <kelechukwuchristwin@gmail.com>
2025-11-21 23:07:19 +0100

    Chore: Started pattern matching 2

--- src/main.rs
+++ src/main.rs
@@ -43,4 +43,11 @@ fn main() {
     while let Ok(value) = rx.recv() {
         println!("{value}");
     }
+    let x = 1;
+    match x {
+        1 => println!("one"),
+        2 => println!("two"),
+        3 => println!("three"),
+        _ => println!("anything"),
+    }
 }
