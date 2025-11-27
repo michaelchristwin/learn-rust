1589e86ca472589dc3ad47921339b8bc84f4839d
michaelchristwin <kelechukwuchristwin@gmail.com>
2025-11-27 06:47:04 +0100

    Chore: Added enum and array destructuring

--- src/main.rs
+++ src/main.rs
@@ -6,6 +6,18 @@ use crate::control_flow::{cf, lp};
 use crate::conversion::{convert_from, convert_into, convert_string, convert_try};
 use crate::ownership::{first_word, ownership};
 
+#[allow(dead_code)]
+enum Color {
+    Red,
+    Blue,
+    Green,
+    RGB(u32, u32, u32),
+    HSV(u32, u32, u32),
+    HSL(u32, u32, u32),
+    CMY(u32, u32, u32),
+    CMYK(u32, u32, u32, u32),
+}
+
 fn main() {
     cf();
     lp();
@@ -50,6 +62,8 @@ fn main() {
         3 => println!("three"),
         _ => println!("anything"),
     }
+
+    // Tuple match and destructure
     let triple = (0, -2, 3);
     println!("Tell me about {:?}", triple);
     match triple {
@@ -58,5 +72,44 @@ fn main() {
         (.., 2) => println!("Last is `2` and the rest don't matter"),
         (3, .., 4) => println!("First is `3`, last is `4` and the rest don't matter"),
         _ => println!("It doesn't matter what they are"),
+    }
+
+    // Array match and destructure
+    let array = [1, -2, 6];
+    match array {
+        [0, second, third] => println!("array[0] = 0, array[1] = {}, array[2] = {}", second, third),
+        [1, _, third] => println!(
+            "array[0] = 1, array[2] = {} and array[1] was ignored",
+            third
+        ),
+        [-1, second, ..] => println!(
+            "array[0] = -1, array[1] = {} and all others will be ignored",
+            second
+        ),
+        [3, second, tail @ ..] => println!(
+            "array[0] = 3, array[1] = {} and the other elements were {:?}",
+            second, tail
+        ),
+        [first, middle @ .., last] => println!(
+            "array[0] = {}, middle = {:?}, array[2] = {}",
+            first, middle, last
+        ),
+    }
+
+    // Enums match and destructure
+    let color = Color::RGB(122, 17, 40);
+    println!("What color is it?");
+    match color {
+        Color::Red => println!("The color is Red!"),
+        Color::Blue => println!("The color is Blue!"),
+        Color::Green => println!("The color is Green!"),
+        Color::RGB(r, g, b) => println!("Red: {}, green: {}, and blue: {}!", r, g, b),
+        Color::HSV(h, s, v) => println!("Hue: {}, saturation: {}, value: {}!", h, s, v),
+        Color::HSL(h, s, l) => println!("Hue: {}, saturation: {}, lightness: {}!", h, s, l),
+        Color::CMY(c, m, y) => println!("Cyan: {}, magenta: {}, yellow: {}!", c, m, y),
+        Color::CMYK(c, m, y, k) => println!(
+            "Cyan: {}, magenta: {}, yellow: {}, key (black): {}!",
+            c, m, y, k
+        ),
     }
 }
