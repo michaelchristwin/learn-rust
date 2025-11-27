2227edd1cae4488d13f886989f9c44717c3298a7
michaelchristwin <kelechukwuchristwin@gmail.com>
2025-11-27 06:07:43 +0100

    Chore: Added tuple destructuring

--- README.md
+++ README.md
@@ -1,6 +1,3 @@
 # The Rust Programming Language
 
-This repo contains all the projects and useful code snippets from the book `The Rust Programming Language`
-My journey to becoming a Rustacean begins here 😎🚀🚀
-
-![The Rust Programming Language](https://m.media-amazon.com/images/I/81j-XMgvndL._SL1500_.jpg)
+This repo contains all the projects and useful code snippets from my journey to becoming a Rustacean 😎🚀🚀
--- src/main.rs
+++ src/main.rs
@@ -50,4 +50,13 @@ fn main() {
         3 => println!("three"),
         _ => println!("anything"),
     }
+    let triple = (0, -2, 3);
+    println!("Tell me about {:?}", triple);
+    match triple {
+        (0, y, z) => println!("First is `0`, `y` is {:?}, and `z` is {:?}", y, z),
+        (1, ..) => println!("First is `1` and the rest don't matter"),
+        (.., 2) => println!("Last is `2` and the rest don't matter"),
+        (3, .., 4) => println!("First is `3`, last is `4` and the rest don't matter"),
+        _ => println!("It doesn't matter what they are"),
+    }
 }
