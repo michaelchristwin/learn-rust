310515ac9a243244ef8a118512d5623f2afbefe1
michaelchristwin <kelechukwuchristwin@gmail.com>
2025-11-28 12:14:52 +0100

    Chore: Added methods

--- src/main.rs
+++ src/main.rs
@@ -170,6 +170,7 @@ fn main() {
         repost: false,
     };
     println!("1 new post: {}", post.summarize());
+    notify(&post);
 
     let article = NewsArticle {
         headline: String::from("Penguins win the Stanley Cup Championship!"),
@@ -181,5 +182,53 @@ fn main() {
         ),
     };
     println!("New article available! {}", article.summarize());
-    //concrete types
+    //Note: concrete types
+
+    // Methods
+    #[derive(Debug)]
+    struct Rectangle {
+        width: u32,
+        height: u32,
+    }
+
+    impl Rectangle {
+        fn area(&self) -> u32 {
+            self.width * self.height
+        }
+
+        fn can_hold(&self, other: &Rectangle) -> bool {
+            self.width > other.width && self.height > other.height
+        }
+
+        //Associated function
+        fn square(size: u32) -> Self {
+            Self {
+                width: size,
+                height: size,
+            }
+        }
+    }
+    let rect1 = Rectangle {
+        width: 30,
+        height: 50,
+    };
+    println!(
+        "The area of the rectangle is {} square pixels",
+        rect1.area()
+    );
+
+    let rect2 = Rectangle {
+        width: 10,
+        height: 40,
+    };
+    let rect3 = Rectangle {
+        width: 60,
+        height: 45,
+    };
+
+    println!("Can rect1 hold rect2? {}", rect1.can_hold(&rect2));
+    println!("Can rect1 hold rect3? {}", rect1.can_hold(&rect3));
+
+    let sq = Rectangle::square(3);
+    println!("The area of this square is {}", sq.area());
 }
